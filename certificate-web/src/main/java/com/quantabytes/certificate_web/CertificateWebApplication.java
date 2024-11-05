f7d7093042e6f7449b3cc0be5ccebbed4456134f
Jai Kumar <116871953+JaiKumarSuther@users.noreply.github.com>
2024-11-05 16:18:45 +0500

    updated a bit

---
+++ certificate-web/src/main/java/com/quantabytes/certificate_web/CertificateWebApplication.java
@@ -0,0 +1,13 @@
+package com.quantabytes.certificate_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateWebApplication.class, args);
+	}
+
+}
